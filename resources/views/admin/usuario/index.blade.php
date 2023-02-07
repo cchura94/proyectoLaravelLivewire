4fafa1bf704b82f9dd09d63191b688a89b4260ef
cchura94 <cchura.cpc@gmail.com>
2023-02-06 22:17:36 -0400

    Theme SBAdmin2

---
+++ resources/views/admin/usuario/index.blade.php
@@ -0,0 +1,21 @@
+@extends("layouts.theme.admin")
+
+@section("titulo", "Gestión Usuarios")
+    
+@section("contenedor")
+
+<div class="row">
+    <div class="col-md-12">
+        
+        <div class="card">
+            <div class="card-body">
+                
+                <h1>Gestión Usuarios</h1>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
+@endsection
